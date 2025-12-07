267f0bc6cbc802376d843765c56f623d70598419
PRANTO <mdpranto417@gmail.com>
2025-12-07 12:23:40 +0600

    fix routes

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Courses from '../pages/Courses';
+import Teachers from '../pages/Teachers';
+import Contact from '../pages/Contact';
+import GetStarted from '../pages/GetStarted';
+import ScrollToTop from '../components/ScrollToTop';
+
+function AppRouter() {
+    return (
+        <Router>
+            <ScrollToTop />
+            <div className="min-h-screen bg-slate-50">
+                <Navbar />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/courses" element={<Courses />} />
+                        <Route path="/teachers" element={<Teachers />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/get-started" element={<GetStarted />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
+}
+
+export default AppRouter;
